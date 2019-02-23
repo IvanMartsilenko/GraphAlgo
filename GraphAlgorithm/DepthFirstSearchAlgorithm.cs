6504a395fed85d68058ae5c57a8735f9292d840e
IvanMartsilenko <ivanmartsilenko@gmail.com>
2019-02-23 21:43:58 +0200

    -added DFS that searching all graph thath started from one Node.

--- GraphAlgorithm/DepthFirstSearchAlgorithm.cs
+++ GraphAlgorithm/DepthFirstSearchAlgorithm.cs
@@ -13,9 +13,28 @@ namespace GraphAlgorithm
         private List<Node> VisitedNodes { get; }
         public DepthFirstSearchAlgorithm(Graph Graph)
         {
+
             InnerGraph = (Graph)Graph.Clone();
             ResultOfSearching = new List<Edge>();
             VisitedNodes = new List<Node>();
+        }
+
+        ///  that type of DFS that start from one Node ( that give as param) and researche all graph
+        /// 
+        /// <param name="Start">< Node that our research start>
+        /// <returns> true if we find all Nodes</returns>
+        public bool DepthFirstSearch(Node Start)
+        {
+            VisitedNodes.Add(Start);
+            for (int Index = 0; Index < InnerGraph.QuantityOfNodes; Index++)
+            {
+                if (InnerGraph.SetOfNodes[Start.Index][Index] != null && !VisitedNodes.Contains(InnerGraph.SetOfNodes[Index]))
+                {
+                    ResultOfSearching.Add(InnerGraph.FindEdge(Start, InnerGraph.SetOfNodes[Index]));
+                    DepthFirstSearch(InnerGraph.SetOfNodes[Index]);
+                }
+            }
+            return VisitedNodes.Count.Equals(InnerGraph.QuantityOfNodes);
         }
     }
 }
