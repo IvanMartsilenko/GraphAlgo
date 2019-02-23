1641bff1f4eb6f6f1b4ee382506271bd57a5749f
IvanMartsilenko <ivanmartsilenko@gmail.com>
2019-02-23 23:07:02 +0200

    -Created class PrimaAlgorithm in file PrimaAlgorithm.cs -added field List of edge for MinimumSpanningTree -added Inner Graph -added List of Nodes for visted Nodes -added constructor

---
+++ GraphAlgorithm/PrimaAlgorithm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphAlgorithm
+{
+    public class PrimaAlgorithm
+    {
+        public List<Edge> MinimumSpanningTree { get; }
+        private static Graph InnerGraph { get; set; }
+        private List<Node> VisitedNodes { get; }
+
+            public PrimaAlgorithm(Graph Graph)
+            {
+                MinimumSpanningTree = new List<Edge>();
+                InnerGraph = (Graph)Graph.Clone();
+                VisitedNodes = new List<Node>();
+            }
+     
+    }
+}
