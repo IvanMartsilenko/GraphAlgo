95b494e9dd592cdc2dc839e359a9b184721a54b2
IvanMartsilenko <ivanmartsilenko@gmail.com>
2019-02-23 18:47:56 +0200

    -added File (with class)  BreadthFirstSearchAlgorithm.cs

---
+++ GraphAlgorithm/BreadthFirstSearchAlgorithm.cs
@@ -0,0 +1,17 @@
+﻿// Created by Ivan Martsilenko K-24
+// Lab 1 Sem 2
+// 1st algo
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphAlgorithm
+{
+    public class BreadthFirstSearchAlgorithm
+    {
+
+    }
+}
