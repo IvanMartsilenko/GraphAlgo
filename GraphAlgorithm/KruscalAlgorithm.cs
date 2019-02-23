ffbbd84e89e6a9af5d021521c5c19dae27e6bc01
IvanMartsilenko <ivanmartsilenko@gmail.com>
2019-02-23 22:37:49 +0200

    - done Kruskal algorithm -added find sub tree -added Kruskal  Searching  Minimum Spanning Tree Algorithm

--- GraphAlgorithm/KruscalAlgorithm.cs
+++ GraphAlgorithm/KruscalAlgorithm.cs
@@ -15,7 +15,47 @@ namespace GraphAlgorithm
             MinimumSpanningTree = new List<Edge>();
             InnerGraph = (Graph)Graph.Clone();
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Root"> start of our research</param>
+        /// <returns></returns>
+        private Node FindSubTree(Node Root)
+        {
+            Node OldNode = new Node();
+            Node RootOfTree = Root;
+            for (; !RootOfTree.Equals(InnerGraph.SetOfNodes[RootOfTree.Index]);)
+            {
+                RootOfTree = InnerGraph.SetOfNodes[RootOfTree.Index];
+            }
+            for (; !Root.Equals(RootOfTree);)
+            {
+                OldNode = InnerGraph.SetOfNodes[Root.Index];
 
+                InnerGraph.SetOfNodes[Root.Index] = RootOfTree;
 
+                Root = OldNode;
+            }
+            return RootOfTree;
+        }
+        /// <summary>
+        /// Using Findsub tree develop from starting tree (root) to minimum spanning tree
+        /// Don't if it should be bool type</summary>
+        /// <returns></returns>
+        public bool KruskalSMSTAlgorithm()
+        {
+            InnerGraph.SetOfEdges = InnerGraph.SetOfEdges.OrderBy(Edge => Edge.Weight).ToList<Edge>();
+            for (int Index = 0; Index < InnerGraph.QuantityOfEdges; Index++)
+            {
+                Node StartNode = FindSubTree(InnerGraph.SetOfEdges[Index][0]);
+                Node EndNode = FindSubTree(InnerGraph.SetOfEdges[Index][1]);
+                if (!StartNode.Equals(EndNode))
+                {
+                    MinimumSpanningTree.Add(InnerGraph.SetOfEdges[Index]);
+                    InnerGraph.SetOfNodes[EndNode.Index] = StartNode;
+                }
+            }
+            return true;
+        }
     }
 }
