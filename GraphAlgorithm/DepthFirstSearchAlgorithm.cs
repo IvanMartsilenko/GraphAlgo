ed3144b185858eaf20766d9e0a67c245e675d1b7
IvanMartsilenko <ivanmartsilenko@gmail.com>
2019-02-23 21:39:08 +0200

    -created class DepthFirstSearchAlgorithm in file DepthFirstSearchAlgorithm.cs - added fields List of visited Nodes and Result way - added inner graph

---
+++ GraphAlgorithm/DepthFirstSearchAlgorithm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphAlgorithm
+{
+    class DepthFirstSearchAlgorithm
+    {
+        public List<Edge> ResultOfSearching { get; }
+        private static Graph InnerGraph { get; set; }
+        private List<Node> VisitedNodes { get; }
+        public DepthFirstSearchAlgorithm(Graph Graph)
+        {
+            InnerGraph = (Graph)Graph.Clone();
+            ResultOfSearching = new List<Edge>();
+            VisitedNodes = new List<Node>();
+        }
+    }
+}
