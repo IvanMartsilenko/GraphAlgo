2ca8d8e72c9eadf83152958ee3694550cd9308fb
IvanMartsilenko <ivanmartsilenko@gmail.com>
2019-02-23 18:26:18 +0200

    take class Coordinate from Program.cs to own file

--- GraphAlgorithm/Program.cs
+++ GraphAlgorithm/Program.cs
@@ -70,26 +70,6 @@ namespace GraphAlgorithm
     }
 
     /// <summary>
-    /// public class Coordinate create field with Decard coordinate system
-    /// </summary>
-    public class Coordinate
-    {
-        public int X { get; private set; }
-        public int Y { get; private set; }
-
-        public Coordinate(int X, int Y)
-        {
-            this.X = X;
-            this.Y = Y;
-        }
-        public Coordinate()
-        {
-            X = 0;
-            Y = 0;
-        }
-    }
-
-    /// <summary>
     /// class Node connected to class Coodinate and class Edge
     /// </summary>
     public class Node : ICloneable
