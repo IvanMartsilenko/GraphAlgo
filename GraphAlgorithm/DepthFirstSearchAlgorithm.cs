14d2e88fc46a856abe520d6878266eb40f723e2f
IvanMartsilenko <ivanmartsilenko@gmail.com>
2019-02-23 21:49:53 +0200

    -added DFS tyoe when we search target and also find way to the target.

--- GraphAlgorithm/DepthFirstSearchAlgorithm.cs
+++ GraphAlgorithm/DepthFirstSearchAlgorithm.cs
@@ -28,13 +28,43 @@ namespace GraphAlgorithm
             VisitedNodes.Add(Start);
             for (int Index = 0; Index < InnerGraph.QuantityOfNodes; Index++)
             {
-                if (InnerGraph.SetOfNodes[Start.Index][Index] != null && !VisitedNodes.Contains(InnerGraph.SetOfNodes[Index]))
+                if (InnerGraph.SetOfNodes[Start.Index][Index] != null 
+                    && !VisitedNodes.Contains(InnerGraph.SetOfNodes[Index]))
                 {
                     ResultOfSearching.Add(InnerGraph.FindEdge(Start, InnerGraph.SetOfNodes[Index]));
                     DepthFirstSearch(InnerGraph.SetOfNodes[Index]);
                 }
             }
             return VisitedNodes.Count.Equals(InnerGraph.QuantityOfNodes);
+        }
+
+        /// <summary>
+        /// that type of DFS that start from one Node ( that give as param) and researche targeted node
+        /// <param name="Start"> first Node in queue</param>
+        /// <param name="Target"> Scope of our researche</param>
+        /// <param name="FoundPath"></param>
+        /// <returns> True if we find target (target in list of visited nodes)</returns>
+        public bool DepthFirstSearch(Node Start, Node Target, Node[] FoundPath)
+        {
+            VisitedNodes.Add(Start);
+            if (VisitedNodes.Contains(Target))
+            {
+                return true;
+            }
+            for (int Index = 0; Index < InnerGraph.QuantityOfNodes; Index++)
+            {
+                if (InnerGraph.SetOfNodes[Start.Index][Index] != null 
+                    && !VisitedNodes.Contains(InnerGraph.SetOfNodes[Index]) 
+                    && InnerGraph.FindEdge(Start, InnerGraph.SetOfNodes[Index]).Weight > 0)
+                {
+                    FoundPath[Index] = Start;
+                    if (DepthFirstSearch(InnerGraph.SetOfNodes[Index], Target, FoundPath))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return VisitedNodes.Contains(Target);
         }
     }
 }
