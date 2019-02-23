fed39783d4b07dc6363ae06721c7e11a6aee24fa
IvanMartsilenko <ivanmartsilenko@gmail.com>
2019-02-23 23:29:56 +0200

    BellmanFoed ALgo added constructor added List of nodes for Shortest Path added InnerGraph

--- GraphAlgorithm/BellmanFordAlgorithm.cs
+++ GraphAlgorithm/BellmanFordAlgorithm.cs
@@ -8,7 +8,12 @@ namespace GraphAlgorithm
 {
     class BellmanFordAlgorithm
     {
-
-   
+        public List<Node> ShortestPath { get; private set; }
+        private static Graph InnerGraph { get; set; }
+        public BellmanFordAlgorithm(Graph Graph)
+        {
+            InnerGraph = (Graph)Graph.Clone();
+            ShortestPath = InnerGraph.SetOfNodes;
+        }
     }
 }
