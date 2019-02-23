3a1f7a5bc9c354ff5c3739b58aa91a6c1ead272c
IvanMartsilenko <ivanmartsilenko@gmail.com>
2019-02-23 22:24:43 +0200

    -Created clas KruscalAlgorithm in file KruscalAlgorithm .cs - added field MinimumSpanningTree as list of Edges - added Inner graph - added constructor

---
+++ GraphAlgorithm/KruscalAlgorithm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphAlgorithm
+{
+    public class KruscalAlgorithm
+    {
+        public List<Edge> MinimumSpanningTree { get; }
+        private static Graph InnerGraph { get; set; }
+        public KruscalAlgorithm(Graph Graph)
+        {
+            MinimumSpanningTree = new List<Edge>();
+            InnerGraph = (Graph)Graph.Clone();
+        }
+
+
+    }
+}
