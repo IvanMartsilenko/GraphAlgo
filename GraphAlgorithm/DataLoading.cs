921a771eeb4d0b31fc9120f07eebf601c22387f4
IvanMartsilenko <ivanmartsilenko@gmail.com>
2019-02-24 23:25:35 +0200

    -added LoadNodes () lazy strategy in class Program -added private static Graph Graph = new Graph();

--- GraphAlgorithm/DataLoading.cs
+++ GraphAlgorithm/DataLoading.cs
@@ -8,11 +8,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace GraphAlgorithm
 {
-    class DataLoading
+    class DataLoading : Program
     {
-
+        public void LoadNodes( Graph GraphEx)
+        {
+            foreach (string line in File.ReadLines(@"C:\Users\Ivan\source\repos\GraphAlgorithm\GraphAlgorithm\bin\GraphData\nodes.txt"))
+            {
+                string[] words = line.Split(new char[] { ' ' }, 4, StringSplitOptions.RemoveEmptyEntries);
+                GraphEx.AddNode(new Node(Convert.ToInt32(words[0]), words[1], new Coordinate(Convert.ToInt32(words[2]), Convert.ToInt32(words[3]))));
+            }
+        }
     }
 }
