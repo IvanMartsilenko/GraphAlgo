5a7f387fe69e7a1cecaca2f1768c76813b6d7d3f
IvanMartsilenko <ivanmartsilenko@gmail.com>
2019-02-24 22:04:44 +0200

    In class A_star.cs -added double HeruisticPath -added A_star algorithm Can be a bug with TotalPathCost if but everything must be ok

--- GraphAlgorithm/A_Star.cs
+++ GraphAlgorithm/A_Star.cs
@@ -5,8 +5,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GraphAlgorithm
 {
@@ -25,6 +23,64 @@ namespace GraphAlgorithm
             ActualNodes = new List<Node>();
             VisitedNodes = new List<Node>();
             ShortesPath = new List<Edge>();
+        }
+
+        private double HeruisticPath(Node Start, Node Target)
+        {
+            return Math.Sqrt(Math.Pow(Start.Coordinate.X - Target.Coordinate.X, 2) + Math.Pow(Start.Coordinate.Y - Target.Coordinate.Y, 2));
+        }
+
+        public bool A_StarAlgorithm(Node Start, Node Target)
+        {
+            Node CurrentNode = Start;
+
+            ActualNodes.Add(CurrentNode);
+
+            for (; ActualNodes.Count > 0;)
+            {
+                CurrentNode = ActualNodes[0];
+
+                if (CurrentNode.Equals(Target))
+                {
+                    VisitedNodes.Add(Target);
+
+                    for (int Index = 0; Index < VisitedNodes.Count - 1; Index++)
+                    {
+                        ShortesPath.Add(InnerGraph.FindEdge(VisitedNodes[Index], VisitedNodes[Index + 1]));
+                    }
+
+                    return true;
+                }
+
+                ActualNodes.Remove(CurrentNode);
+
+                ClosedNodes.Add(CurrentNode);
+
+                foreach (Node Incomer in CurrentNode.Incomers.Where(Node => Node != null && Node.Index != Node.Incomers.Length - 1))
+                {
+                    if (!ClosedNodes.Contains(Incomer))
+                    {
+                        if (!ActualNodes.Contains(Incomer))
+                        {
+                            Incomer[Incomer.Index] = CurrentNode;
+
+                            Incomer.HeuristicCost = HeruisticPath(Incomer, Target);
+
+                            Incomer.PastWayCost = InnerGraph.FindEdge(CurrentNode, Incomer).Weight;
+
+                            Incomer.TotalPathCost = Incomer.PastWayCost + Incomer.HeuristicCost;
+
+                            ActualNodes.Add(Incomer);
+
+                            ActualNodes = ActualNodes.OrderBy(Node => Node.TotalPathCost).ToList<Node>();
+                        }
+                    }
+                }
+
+                VisitedNodes.Add(CurrentNode);
+            }
+
+            return true;
         }
     }
 }
