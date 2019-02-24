c217c88f88a0e5d50b4d089e65ddeb61641fd3c3
IvanMartsilenko <ivanmartsilenko@gmail.com>
2019-02-24 14:33:09 +0200

    -added public bool DijkstraSWSAlgorithm(Node Start)  means  Dijkstra Shortest Way Search Algorithm -deleted field Index

--- GraphAlgorithm/DijkstraAlgorithm.cs
+++ GraphAlgorithm/DijkstraAlgorithm.cs
@@ -15,19 +15,44 @@ namespace GraphAlgorithm
         public List<Node> ShortestPath { get; private set; }
         private List<Node> VisitedNodes { get; }
         private static Graph InnerGraph { get; set; }
-        private int Index { get; set; }
-
 
         public DijkstraAlgorithm(Graph Graph)
         {
             InnerGraph = (Graph)Graph.Clone();
-
             ShortestPath = InnerGraph.SetOfNodes;
-
             VisitedNodes = new List<Node>();
-
             SetInfinityWeightsForNode(ShortestPath);
         }
+        public bool DijkstraSWSAlgorithm(Node Start)
+        {
+            ShortestPath[Start.Index].Weight = 0;
 
+            for (int FirstIndex = 0; FirstIndex < InnerGraph.QuantityOfNodes - 1; FirstIndex++)
+            {
+                Node CurrentNode = new Node { Weight = double.PositiveInfinity };
+                for (int SecondIndex = 0; SecondIndex < InnerGraph.QuantityOfNodes; SecondIndex++)
+                {
+                    if (!VisitedNodes.Contains(ShortestPath[SecondIndex]) && ShortestPath[SecondIndex].Weight <= CurrentNode.Weight)
+                    {
+                        CurrentNode = ShortestPath[SecondIndex];
+                        Index = ShortestPath[SecondIndex].Index;
+                    }
+                }
+                VisitedNodes.Add(ShortestPath[Index]);
+                for (int SecondIndex = 0; SecondIndex < InnerGraph.QuantityOfNodes; SecondIndex++)
+                {
+                    if (!VisitedNodes.Contains(ShortestPath[SecondIndex]) && InnerGraph.SetOfNodes[Index][SecondIndex] != null 
+                        && !ShortestPath[Index].Weight.Equals(double.PositiveInfinity) 
+                        && ShortestPath[SecondIndex].Weight > 
+                        ShortestPath[Index].Weight + InnerGraph.FindEdge(InnerGraph.SetOfNodes[Index], InnerGraph.SetOfNodes[SecondIndex]).Weight)
+
+                    {
+                    ShortestPath[SecondIndex].Weight = ShortestPath[Index].Weight + InnerGraph.FindEdge(InnerGraph.SetOfNodes[Index], InnerGraph.SetOfNodes[SecondIndex]).Weight;
+                    }
+                }
+            }
+
+            return InnerGraph.NegativeCycleChecker(ShortestPath);
+        }
     }
     }
