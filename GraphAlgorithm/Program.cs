7fae1c21ff0d34b868bbaea4a8cec4a3d80c4d11
IvanMartsilenko <ivanmartsilenko@gmail.com>
2019-02-24 23:56:27 +0200

    In class program -added field BellmanFord of class BellmanFordAlgorithm -added field Dijkstra of class DijkstraAlgorithm -added field Floyd_Warshall of class Floyd_WarshallAlgorithm -added field Johnson of class JohnsonAlgorithm -added field FordFulkerson of class FordFulkersonAlgorithm -added field A_Star of class A_Star

--- GraphAlgorithm/Program.cs
+++ GraphAlgorithm/Program.cs
@@ -19,6 +19,13 @@ namespace GraphAlgorithm
         private static DepthFirstSearchAlgorithm DFS = new DepthFirstSearchAlgorithm(Graph);
         private static KruscalAlgorithm Kruscal = new KruscalAlgorithm(Graph);
         private static PrimaAlgorithm Prima = new PrimaAlgorithm(Graph);
+        private static BellmanFordAlgorithm BellmanFord = new BellmanFordAlgorithm(Graph);
+        private static DijkstraAlgorithm Dijkstra = new DijkstraAlgorithm(Graph);
+        private static Floyd_WarshallAlgorithm Floyd_Warshall = new Floyd_WarshallAlgorithm(Graph);
+        private static JohnsonAlgorithm Johnson = new JohnsonAlgorithm(Graph);
+        private static FordFulkersonAlgorithm FordFulkerson = new FordFulkersonAlgorithm(Graph);
+        private static A_Star A_Star = new A_Star(Graph);
+
         static void Main(string[] args)
         {
             Load.LoadNodes(Graph);
