7a10b9798fe2f2149465d89b425292eef4dfdc00
IvanMartsilenko <ivanmartsilenko@gmail.com>
2019-02-24 16:37:09 +0200

    In class Johnson Algorithm -added Dijkstar Algorithm field -added InnerGraph -added MatrixOfTheShortestPathes - added constuctor

--- GraphAlgorithm/JohnsonAlgorithm.cs
+++ GraphAlgorithm/JohnsonAlgorithm.cs
@@ -8,5 +8,14 @@ namespace GraphAlgorithm
 {
     class JohnsonAlgorithm
     {
+        private static DijkstraAlgorithm DijkstraPathSearch { get; set; }
+        private static Graph InnerGraph { get; set; }
+        public double[,] MatrixOfTheShortesPathes { get; private set; }
+
+        public JohnsonAlgorithm(Graph Graph)
+        {
+            MatrixOfTheShortesPathes = new double[Graph.QuantityOfNodes, Graph.QuantityOfNodes];
+            InnerGraph = (Graph)Graph.Clone();
+        }
     }
 }
